d2dcb9f018f406b62a8df0c07fdd4e64a9246fb9
Hamgule <hsj6831@naver.com>
2021-09-03 18:50:43 +0900

    second commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -1,10 +1,16 @@
 package com.example.lab1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!");
+		System.out.print("Enter your name : ");
+		String name = s.next();
+		System.out.println("Welcome [" + name + "] ~");
+		
 	}
 
 }
